827958660b588489c3ab45664e5deb647b70e3d3
Bestcxy <snailknight@yeah.net>
2017-05-31 18:03:12 +0800

    按照唯一性所有，mybitas会自动获取唯一一个，如果结果有多个会报错

--- src/main/java/com/bestcxx/stu/springmvc/mapper/UserModelMapper.java
+++ src/main/java/com/bestcxx/stu/springmvc/mapper/UserModelMapper.java
@@ -11,7 +11,8 @@ import com.bestcxx.stu.springmvc.model.UserModel;
  * @Datetime 2017年5月2日
  */
 public interface UserModelMapper {
-	public UserModel getUserModel(@Param("userName") String userName);
+	public UserModel getUserModel(@Param("userName") String userName);//搜索返回一个实体,如果有多个则报错
 	public int addUserModel(UserModel userModel );//如果启动 mybitas 的属性自识别功能，不要对实体进行@Param("userModel")的设置
 	public ArrayList<UserModel> getUserModelListByStrs(@Param("userNames") String userNames);//入参为 11,22,33 形式的字符串，返回实体的list
+	
 }
