ddc8944880b17744b35e0108995936a688312be7
wujie <snailknight@yeah.net>
2017-06-24 11:15:17 +0800

    notify和interrupt都不会使本线程立即中断，而是运行结束后再释放临界区

--- src/test/java/com/bestcxx/stu/springmvc/thread/WaitNotifyTest.java
+++ src/test/java/com/bestcxx/stu/springmvc/thread/WaitNotifyTest.java
@@ -4,7 +4,8 @@ import com.bestcxx.stu.springmvc.model.UserModel;
 
 //并发同步的情况下
 //Object wait 本线程阻塞，同级线程开始运行，notify 本线程结束，被阻塞的开始运行
-//wait 阻塞时，本线程和其余线程无差异
+//wait 阻塞时，本线程和其余线程无差异,调用wait的线程在再次获得临界区上并没有特殊权重
+//wait不会使本线程退出，notify不会使本线程立即退出，前者等待其他线程调用 notify 释放临界区后继续运行（在阻塞线程中等待），后者则在剩余代码运行结束后才正式释放临界区并结束线程
 public class WaitNotifyTest {
 	private static UserModel userModel=new UserModel();
 	public static void main(String[] args) {
@@ -67,7 +68,7 @@ public class WaitNotifyTest {
 		
 		t0.start();
 		t1.start();
-		t2.start();
+		//t2.start();-放开此代码可以发现，调用wait的线程在再次获得临界区上并没有特殊权重
 	}
 	
 	
